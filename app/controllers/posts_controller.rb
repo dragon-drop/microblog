960c17bde1c8435641ca9c2b8b411d02ef0d2c6e
adamdawkins <adam@dragondrop.uk>
2023-02-07 22:21:17 +0000

    Rubocop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
     @posts = Post.all.order(created_at: :desc)
   end
 
-  def create
+  def create # rubocop:disable Metrics/AbcSize
     @post = Post.new(post_params.merge(user: current_user))
     respond_to do |format|
       if @post.save
