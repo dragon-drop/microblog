227c96d9b8c2056ba7f17fe465872f0130bc9859
Will H <will.hodgson@hotmail.co.uk>
2023-02-28 11:26:13 +0000

    Fixup to Like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,12 @@ class Like < ApplicationRecord
     belongs_to :user
     belongs_to :post
 
-    validates :user_id, uniqueness: {scope: :post_id}
+    validates_uniqueness_of :user_id, scope: :post_id
+    validate :user_cannot_be_post_owner
+
+    def user_cannot_be_post_owner
+      errors.add(:user, "Cannot like their own post") if user_id == Post.find(post_id).user_id
+    end
 end
   
 # == Schema Information
